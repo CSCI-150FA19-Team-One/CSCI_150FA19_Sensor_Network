99801f0aeef5a3ac9f46c3c2ffec5e20c7bede72
Gilbert Barr <gbarr62@yahoo.com>
2019-09-28 09:07:59 -0700

    Update particleRequestTest.py

--- CSCI_150_Hardware/Test_Scripts/particleRequestTest.py
+++ CSCI_150_Hardware/Test_Scripts/particleRequestTest.py
@@ -1,9 +1,11 @@
-#device 50ff6f065067545626430587
-#token  895043e69c01b80b464cdd995e913efa4b25c3a3
+#device ID -- 50ff6f065067545626430587
+#access token -- 895043e69c01b80b464cdd995e913efa4b25c3a3
 #curl "https://api.particle.io/v1/devices/50ff6f065067545626430587/temp?access_token=895043e69c01b80b464cdd995e913efa4b25c3a3"
 
 import requests
 import json
 r = requests.get("https://api.particle.io/v1/devices/50ff6f065067545626430587/temp?access_token=895043e69c01b80b464cdd995e913efa4b25c3a3")
-r.json()
-print(r.json())
+webresult = json.loads(r.text)
+print(webresult)
+print('\n')
+print(webresult['result'])
