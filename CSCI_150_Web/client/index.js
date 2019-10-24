fc989b09eff997bac0860f4257a2e0888c06cc3f
jasonluc1 <39607046+jasonluc1@users.noreply.github.com>
2019-10-24 15:47:52 -0700

    front-end1
    tab.js - Created a tab component which will track which tab is being viewed and allows user to switch between tabs
    
    index.js - Creates a temporary component to hold the tabs

---
+++ CSCI_150_Web/client/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+//Testing tabs of different graphs
+function Graph() {
+    return (
+      <div>
+        <h1>Tabs Demo</h1>
+      </div>
+    );
+  }
+  
+  const container = document.createElement('div');
+  document.body.appendChild(container);
+  render(<App />, container);
