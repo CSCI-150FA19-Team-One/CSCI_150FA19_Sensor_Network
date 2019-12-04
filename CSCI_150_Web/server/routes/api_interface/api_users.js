bdfd395b142917e45f7013b4e936162ea9dbc8d1
Richard <25515721+RichardMoj@users.noreply.github.com>
2019-12-04 08:57:17 -0800

    fixed issues when no username/password provided, error for register with exist username

--- CSCI_150_Web/server/routes/api_interface/api_users.js
+++ CSCI_150_Web/server/routes/api_interface/api_users.js
@@ -15,28 +15,33 @@ const SecretKey = "nodesensor";
 //create a new user
 router.post('/register', jsonParser,  (req, res) => {
 
-	bcrypt.genSalt(Salts, (err, salt) => {
-		if(err) { return res.status(400).send(err);}
+	if( req.body.username != null && req.body.password != null){
+		bcrypt.genSalt(Salts, (err, salt) => {
+			if(err) { return res.status(500).json({"error": "could not generate salt"});}
+			else{
+				bcrypt.hash(req.body.password, salt, (err, hash) => {
+					if(err){ return res.status(500).json({"error": "could not hash password!"});}
+					else{
 
-		bcrypt.hash(req.body.password, salt, (err, hash) => {
-			if(err) { return res.status(500).send(err);}
+						const user = new User({
+							username: req.body.username,
+							password: hash
+						});
 
-			const user = new User({
-				username: req.body.username,
-				password: hash
-			});
-
-			//If user already exists in the database, it will return err
-			user.save((err, docs) => {
-				if(err) {return res.sendStatus(500);}
-				else return res.status(200).send(docs);
-			});
-
-		});
-
-
-	});
+						user.save((err, docs) => {
+							if(err){ return res.status(500).json({"error": "username already exists!"});}
+							return res.status(200).send(docs);
+						});
+					}
+				}); //end of bcrypt hash
+			}
+		}); //end of bcrypt genSalt aysnc
+	}
+	else{
+		return res.status(500).json({"error": "missing password or username!"});
+	}
 });
+
 
 
 
@@ -44,47 +49,53 @@ router.post('/register', jsonParser,  (req, res) => {
 //Then checking if supplied plaintext password is the same
 //as the hashed one.
 router.post('/login', jsonParser,  (req, res) => {
-	filter = {
-		username: req.body.username
-	}
 
-
-	User.findOne(filter, (err, docs) => {
-		if(err) {			
-			return res.status(422);
+	if( req.body.username != null && req.body.password != null){
+		
+		filter = {
+			username: req.body.username
 		}
 
-		console.log("found matching document with match username!");
-		const hashedPW = docs.password;
-
-
-
-		//If passwords match, the result is set to True
-		bcrypt.compare(req.body.password, hashedPW, (err, result) => {
-			if(result) {
-
-				//create a token
-				const token = jwt.sign(
-					{username: docs.username}, 
-					SecretKey,
-					{expiresIn: "90 minutes"},
-					);
-
-				//place token into the database
-				User.updateOne(filter, {$set: {'tokens': {token: token}}}, (err, result) => {
-						if(err){
-							console.log("There was an error inserting new token!");
-							return res.status(421).json({"error": "error updating token value"});
-						}
-						return res.status(200).json({token: token});
-					});
+		User.findOne(filter, (err, docs) => {
+			if(err) {			
+				return res.status(422);
 			}
-			else{
-				return res.status(500).json({"error": "passwords did not match!"});
-			}
-		}); //end of bcrypt
 
-	}); //end of findOne
+			console.log("found matching document with match username!");
+			const hashedPW = docs.password;
+
+
+
+			//If passwords match, the result is set to True
+			bcrypt.compare(req.body.password, hashedPW, (err, result) => {
+				if(result) {
+
+					//create a token
+					const token = jwt.sign(
+						{username: docs.username}, 
+						SecretKey,
+						{expiresIn: "90 minutes"},
+						);
+
+					//place token into the database
+					User.updateOne(filter, {$set: {'tokens': {token: token}}}, (err, result) => {
+							if(err){
+								console.log("There was an error inserting new token!");
+								return res.status(421).json({"error": "error updating token value"});
+							}
+							return res.status(200).json({token: token});
+						});
+				}
+				else{
+					return res.status(500).json({"error": "passwords did not match!"});
+				}
+			}); //end of bcrypt
+
+		}); //end of findOne
+	}
+	else{
+		return res.status(500).json({"error": "missing either username or password"});
+	}
 
 
 });//end of post login api
